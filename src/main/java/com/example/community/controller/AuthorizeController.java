73615ca685f19ed8ed321166ac747011f5b18a55
hubin <1426261937@qq.com>
2020-01-17 19:19:34 +0800

    jsdfkl

---
+++ src/main/java/com/example/community/controller/AuthorizeController.java
@@ -0,0 +1,54 @@
+package com.example.community.controller;
+
+import com.example.community.dto.AccessTokenDTO;
+import com.example.community.dto.GithubUser;
+import com.example.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.HttpRequestHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+
+
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code, @RequestParam(name="state") String state, HttpServletRequest request) {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        //System.out.println(user.getName());
+        if(user != null){
+            //登录成功，
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        }else{
+            //登录失败
+            return "redirect:/";
+        }
+
+    }
+}
